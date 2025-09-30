9570319ff3db8298329b0724bdf18c7ba8dc3f3d
s2847547 <s2847547@students.fscj.edu>
2025-09-30 18:09:12 -0400

    Training a model with Create ML

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,19 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(Date.now.formatted(date: .long, time: .shortened))
-    }
-    
-    func exampleDates() {
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? .now
-        
-        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
-        
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
     }
 }
 
